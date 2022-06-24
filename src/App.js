68b13b829d1ffe3968a0bf72230357b9b81b76d1
LaynMDream <layn_mdream_@hotmail.com>
2022-06-24 12:30:58 +0200

    debut projet ile de paix

--- src/App.js
+++ src/App.js
@@ -5,18 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='flex justify-center'>
+        <p className='text-red-600 text-2xl'>Welcome to ile de paix</p>
+      </div>
       </header>
     </div>
   );
