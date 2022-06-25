f73d772c98870ce6d044a854029b374307533294
Urkral <esteban.dutrannois@outlook.com>
2022-06-25 23:36:18 +0200

    bite

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+return(
+    <div className="bg-[#b27d71]">
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+)
+}
+export default Footer 
