926abc7f9e73196be7082747b1841c9a42a2389d
LaynMDream <layn_mdream_@hotmail.com>
2022-06-26 01:26:23 +0200

    Routes

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import IsConnectContext from "./services/isConnect.context";
 import ConnectForm from "./components/ConnectForm";
 import Logout from "./pages/Logout";
+import AdminForm from "./components/AdminForm";
+import AdminCreate from "./components/AdminCreate";
+import FormNewNGO from "./components/FormNewNGO";
+import ListInfo from "./components/ListInfo";
+import UpdatePasswordForm from "./components/UpdatePasswordForm";
 
 function App() {
   const [isConnect, setIsConnect] = useState(false);
@@ -27,6 +32,14 @@ function App() {
               <Route path="FormTool" element={<FormTool />} />
               <Route path="ConnectForm" element={<ConnectForm />} />
               <Route path="logout" element={<Logout />} />
+              <Route path="AdminForm" element={<AdminForm />} />
+              <Route path="AdminCreate" element={<AdminCreate />} />
+              <Route path="FormNewNGO" element={<FormNewNGO />} />
+              <Route path="ListInfo" element={<ListInfo />} />
+              <Route
+                path="UpdatePasswordForm"
+                element={<UpdatePasswordForm />}
+              />
             </Routes>
           </main>
         </div>
